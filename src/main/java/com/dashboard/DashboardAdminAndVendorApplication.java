45a11512def7ecf4b519a38606d59bf130985711
Krishna <gkrrish.11@gmail.com>
2024-07-16 02:18:41 +0530

    initial commit

---
+++ src/main/java/com/dashboard/DashboardAdminAndVendorApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardAdminAndVendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardAdminAndVendorApplication.class, args);
+	}
+
+}
